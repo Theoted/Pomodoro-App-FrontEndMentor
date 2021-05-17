7e3bf7b185c999e577966b0b130db57c11fb0a6f
Theoted <theodeville@yahoo.fr>
2021-05-17 22:20:42 +0200

    Repo file updated

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Pomodoro app solution
+
+This is a solution to the [Pomodoro app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pomodoro-app-KBFnycJ6G). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Set a pomodoro timer and short & long break timers
+- Customize how long each timer runs for
+- See a circular progress bar that updates every minute and represents how far through their timer they are
+- Customize the appearance of the app with the ability to set preferences for colors and fonts
+
+### Screenshot
+
+![Pomodoro App Screen](/pomodoroScreen.png)
+
+### Links
+
+Not published yet !
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Vanilla JavaScript
+
+### What I learned
+
+This project is THE project that allowed me to make my mark in pure JavaScript. 
+
+Now I consider that I have the basics in JS even if the writing is of course to work on. I will be able to continue on more technical projects and potentially start learning framwork js.
+
+
+### Continued development
+
+The thing I need to work on is the writing and cleanliness of my code whether it is in css or in JS. 
+
+
+### Useful resources
+
+- [Most important ressource](https://www.youtube.com/watch?v=t7eHSAXW718) - This is THE ressource who helped me a lot. With this video I learned how to create a circle in svg and that was the most important part even if it was far from being the most difficult !
+
+
+## Author
+
+- Website - [No website yet !](https://www.no-site-yet.no)
+- Frontend Mentor - [@Theoted](https://www.frontendmentor.io/profile/Theoted)
+- Twitter - [@nasty697](https://twitter.com/nasty_697)
---
+++ public/app.js
@@ -0,0 +1,513 @@
+// Selector
+//Timer
+let totalSeconds;
+let totalSecondsShort;
+let totalSecondsLong;
+let timer;
+let timerShort;
+let timerLong;
+let pauseEl = 1; // if main timer is in pause = 1 else = 0
+let timersLunched = 0; // if this var = 1, chevrons cant change inputs value
+let mainTimerFinished = 0; // if main timer completely finished, this var = 1;
+let menuUp = 0; // if menu is up = 1
+
+//MainPage selectors
+const countDownEl = document.querySelectorAll(".time");
+const circle = document.querySelector("circle");
+const mainButtons = document.querySelectorAll(".mainBtn");
+const btnContainer = document.querySelector(".btnContainer");
+const pauseRestartBtn = document.querySelectorAll("div.pauseRestart");
+const timers = document.querySelectorAll(".timers");
+
+//Menu selectors
+const applyMenuBtn = document.querySelector(".applyMenuBtn");
+const menuContainer = document.querySelector(".menuContainer");
+const settingBtn = document.querySelector(".fa-cog");
+const leaveMenuBtn = document.querySelector(".fa-times");
+const inputs = document.querySelectorAll("input");
+const chevronsUp = document.querySelectorAll(".fa-chevron-up");
+const chevronsDown = document.querySelectorAll(".fa-chevron-down");
+const fontButtons = document.querySelectorAll(".fontButtons");
+const colorButtons = document.querySelectorAll(".colorButtons");
+const colorButtonsFas = document.querySelectorAll(".buttonFas");
+
+//When app open
+colorButtonsFas[0].classList.add("fa-check");
+mainButtons[0].classList.add("mainBtnStyle");
+fontButtons[0].classList.add("fontSelectorBg");
+
+//Event listener
+
+// Style
+settingBtn.addEventListener("click", () => {
+  menuContainer.classList.toggle("menuMove");
+  settingBtn.classList.toggle("cogAnimation");
+});
+
+leaveMenuBtn.addEventListener("click", () => {
+  menuContainer.classList.remove("menuMove");
+});
+
+// Style for font selector button on click
+
+document.addEventListener("click", (e) => {
+  if (e.target === fontButtons[0]) {
+    fontButtons[0].classList.add("fontSelectorBg");
+    fontButtons[1].classList.remove("fontSelectorBg");
+    fontButtons[2].classList.remove("fontSelectorBg");
+  } else if (e.target === fontButtons[1]) {
+    fontButtons[1].classList.add("fontSelectorBg");
+    fontButtons[0].classList.remove("fontSelectorBg");
+    fontButtons[2].classList.remove("fontSelectorBg");
+  } else if (e.target === fontButtons[2]) {
+    fontButtons[2].classList.add("fontSelectorBg");
+    fontButtons[0].classList.remove("fontSelectorBg");
+    fontButtons[1].classList.remove("fontSelectorBg");
+  }
+});
+
+// Style for color buttons selector check
+document.addEventListener("click", (e) => {
+  for (let i = 0; i < colorButtons.length; i++) {
+    if (e.target === colorButtons[i] || e.target === colorButtonsFas[i]) {
+      colorButtonsFas[i].classList.toggle("fa-check");
+    }
+  }
+
+  if (e.target === colorButtons[0]) {
+    colorButtonsFas[1].classList.remove("fa-check");
+    colorButtonsFas[2].classList.remove("fa-check");
+  } else if (e.target === colorButtons[1]) {
+    colorButtonsFas[0].classList.remove("fa-check");
+    colorButtonsFas[2].classList.remove("fa-check");
+  } else if (e.target === colorButtons[2]) {
+    colorButtonsFas[0].classList.remove("fa-check");
+    colorButtonsFas[1].classList.remove("fa-check");
+  }
+});
+
+// Change timers
+mainButtons[0].addEventListener("click", () => {
+  timers[0].style.display = "flex";
+  timers[1].style.display = "none";
+  timers[2].style.display = "none";
+  stopShortBreak();
+  stopLongBreak();
+});
+
+mainButtons[1].addEventListener("click", () => {
+  timers[0].style.display = "none";
+  timers[1].style.display = "flex";
+  timers[2].style.display = "none";
+  startShort();
+});
+
+mainButtons[2].addEventListener("click", () => {
+  timers[0].style.display = "none";
+  timers[1].style.display = "none";
+  timers[2].style.display = "flex";
+  startLong();
+});
+
+// Set up main buttons
+mainButtons[0].addEventListener("click", runTimer);
+mainButtons[1].addEventListener("click", pauseTimer);
+mainButtons[2].addEventListener("click", pauseTimer);
+
+// Lunch timer when applyMenuBtn is clicked
+applyMenuBtn.addEventListener("click", () => {
+  menuContainer.classList.remove("menuMove");
+  pauseRestartBtn[0].innerHTML = "PAUSE";
+  circle.style.strokeDashoffset = 900;
+  mainTimerFinished = 0;
+  timersLunched = 1;
+  start();
+
+  // change style
+
+  // Change first main buttons color
+  if (colorButtonsFas[0].classList.contains("fa-check")) {
+    mainButtons[0].classList.remove("mainBtnStyleBlue");
+    mainButtons[0].classList.remove("mainBtnStylePurple");
+    mainButtons[0].classList.add("mainBtnStyle");
+  } else if (colorButtonsFas[1].classList.contains("fa-check")) {
+    mainButtons[0].classList.remove("mainBtnStyle");
+    mainButtons[0].classList.add("mainBtnStyleBlue");
+  } else if (colorButtonsFas[2].classList.contains("fa-check")) {
+    mainButtons[0].classList.remove("mainBtnStyle");
+    mainButtons[0].classList.add("mainBtnStylePurple");
+  }
+  // color
+  if (colorButtonsFas[0].classList.contains("fa-check")) {
+    circle.style.stroke = "#f87070";
+  } else if (colorButtonsFas[1].classList.contains("fa-check")) {
+    circle.style.stroke = "#70F3F8";
+  } else if (colorButtonsFas[2].classList.contains("fa-check")) {
+    circle.style.stroke = "#D881F8";
+  }
+
+  // font
+  if (fontButtons[0].classList.contains("fontSelectorBg")) {
+    for (let i = 0; i < timers.length; i++) {
+      timers[i].style.fontFamily = '"Kumbh Sans", sans-serif';
+      timers[i].style.marginBottom = "0";
+      mainButtons[i].style.fontFamily = '"Kumbh Sans", sans-serif';
+    }
+  } else if (fontButtons[1].classList.contains("fontSelectorBg")) {
+    for (let i = 0; i < timers.length; i++) {
+      timers[i].style.fontFamily = '"Roboto Slab", sans-serif';
+      timers[i].style.marginBottom = "1rem";
+      mainButtons[i].style.fontFamily = '"Roboto Slab", sans-serif';
+    }
+  } else if (fontButtons[2].classList.contains("fontSelectorBg")) {
+    for (let i = 0; i < timers.length; i++) {
+      timers[i].style.fontFamily = '"Space Mono", sans-serif';
+      timers[i].style.marginBottom = "1rem";
+      mainButtons[i].style.fontFamily = '"Space Mono", sans-serif';
+    }
+  }
+});
+
+// Set main buttons color
+btnContainer.addEventListener("click", (e) => {
+  if (colorButtonsFas[0].classList.contains("fa-check")) {
+    for (let i = 0; i < mainButtons.length; i++) {
+      if (e.target === mainButtons[i]) {
+        mainButtons[i].classList.add("mainBtnStyle");
+      }
+    }
+    if (e.target === mainButtons[0]) {
+      mainButtons[0].classList.add("mainBtnStyle");
+      mainButtons[1].classList.remove("mainBtnStyle");
+      mainButtons[2].classList.remove("mainBtnStyle");
+    } else if (e.target === mainButtons[1]) {
+      mainButtons[1].classList.add("mainBtnStyle");
+      mainButtons[0].classList.remove("mainBtnStyle");
+      mainButtons[2].classList.remove("mainBtnStyle");
+    } else if (e.target === mainButtons[2]) {
+      mainButtons[2].classList.add("mainBtnStyle");
+      mainButtons[0].classList.remove("mainBtnStyle");
+      mainButtons[1].classList.remove("mainBtnStyle");
+    }
+  } else if (colorButtonsFas[1].classList.contains("fa-check")) {
+    for (let i = 0; i < mainButtons.length; i++) {
+      if (e.target === mainButtons[i]) {
+        mainButtons[i].classList.add("mainBtnStyleBlue");
+      }
+    }
+    if (e.target === mainButtons[0]) {
+      mainButtons[0].classList.add("mainBtnStyleBlue");
+      mainButtons[1].classList.remove("mainBtnStyleBlue");
+      mainButtons[2].classList.remove("mainBtnStyleBlue");
+    } else if (e.target === mainButtons[1]) {
+      mainButtons[1].classList.add("mainBtnStyleBlue");
+      mainButtons[0].classList.remove("mainBtnStyleBlue");
+      mainButtons[2].classList.remove("mainBtnStyleBlue");
+    } else if (e.target === mainButtons[2]) {
+      mainButtons[2].classList.add("mainBtnStyleBlue");
+      mainButtons[0].classList.remove("mainBtnStyleBlue");
+      mainButtons[1].classList.remove("mainBtnStyleBlue");
+    }
+  } else if (colorButtonsFas[2].classList.contains("fa-check")) {
+    for (let i = 0; i < mainButtons.length; i++) {
+      if (e.target === mainButtons[i]) {
+        mainButtons[i].classList.add("mainBtnStylePurple");
+      }
+    }
+    if (e.target === mainButtons[0]) {
+      mainButtons[0].classList.add("mainBtnStylePurple");
+      mainButtons[1].classList.remove("mainBtnStylePurple");
+      mainButtons[2].classList.remove("mainBtnStylePurple");
+    } else if (e.target === mainButtons[1]) {
+      mainButtons[1].classList.add("mainBtnStylePurple");
+      mainButtons[0].classList.remove("mainBtnStylePurple");
+      mainButtons[2].classList.remove("mainBtnStylePurple");
+    } else if (e.target === mainButtons[2]) {
+      mainButtons[2].classList.add("mainBtnStylePurple");
+      mainButtons[0].classList.remove("mainBtnStylePurple");
+      mainButtons[1].classList.remove("mainBtnStylePurple");
+    }
+  }
+});
+
+// Change inputs values with chevrons
+document.addEventListener("click", (e) => {
+  if (timersLunched === 0) {
+    for (let i = 0; i < chevronsUp.length; i++) {
+      if (e.target === chevronsUp[i]) {
+        if (inputs[i].value === "") {
+          break;
+        } else {
+          inputs[i].value = parseInt(inputs[i].value) + 1;
+        }
+      } else if (e.target === chevronsDown[i]) {
+        if (inputs[i].value === "") {
+          break;
+        } else {
+          inputs[i].value = parseInt(inputs[i].value) - 1;
+          if (inputs[i].value < 1) {
+            inputs[i].value = 0;
+          }
+        }
+      }
+    }
+  }
+});
+
+// Set up pause button below timer
+pauseRestartBtn[0].addEventListener("click", () => {
+  if (mainTimerFinished === 0) {
+    if (pauseEl === 0) {
+      pauseTimer();
+      pauseRestartBtn[0].innerHTML = "RESTART";
+    } else if (pauseEl === 1) {
+      runTimer();
+      pauseRestartBtn[0].innerHTML = "PAUSE";
+    }
+  } else if (mainTimerFinished === 1) {
+    pauseRestartBtn[0].innerHTML = "PAUSE";
+    circle.style.strokeDashoffset = 900;
+    start();
+    mainTimerFinished = 0;
+  }
+});
+
+document.addEventListener("click", () => {
+  console.log();
+});
+
+// Functions
+
+// Global functions
+
+function getMinutes(totalSeconds) {
+  let minutes = Math.floor(totalSeconds / 60);
+  return minutes < 10 ? "0" + minutes : minutes; // Gets quotient rounded down
+}
+
+function getSeconds(totalSeconds) {
+  let seconds = totalSeconds % 60; // Gets remainder after division
+  return seconds < 10 ? "0" + seconds : seconds; // Inserts "0" if needed
+}
+
+function disable(element) {
+  element.setAttribute("disabled", "");
+}
+function enable(element) {
+  element.removeAttribute("disabled");
+}
+
+function allDisable() {
+  disable(inputs[0]);
+  disable(inputs[1]);
+  disable(inputs[2]);
+  disable(fontButtons[0]);
+  disable(fontButtons[1]);
+  disable(fontButtons[2]);
+  disable(colorButtons[0]);
+  disable(colorButtons[1]);
+  disable(colorButtons[2]);
+  disable(applyMenuBtn);
+}
+function allEnable() {
+  enable(inputs[0]);
+  enable(inputs[1]);
+  enable(inputs[2]);
+  enable(fontButtons[0]);
+  enable(fontButtons[1]);
+  enable(fontButtons[2]);
+  enable(colorButtons[0]);
+  enable(colorButtons[1]);
+  enable(colorButtons[2]);
+  enable(applyMenuBtn);
+}
+
+function disableMainButtons() {
+  disable(mainButtons[0]);
+  disable(mainButtons[1]);
+  disable(mainButtons[2]);
+  mainButtons[0].style.cursor = "default";
+  mainButtons[1].style.cursor = "default";
+  mainButtons[2].style.cursor = "default";
+}
+function enableMainButtons() {
+  enable(mainButtons[0]);
+  enable(mainButtons[1]);
+  enable(mainButtons[2]);
+  mainButtons[0].style.cursor = "pointer";
+  mainButtons[1].style.cursor = "pointer";
+  mainButtons[2].style.cursor = "pointer";
+}
+
+//MAIN TIMER FUNCTIONS
+
+function start() {
+  totalSeconds = inputs[0].value * 60; // Sets initial value of totalSeconds based on user input
+  countDownEl[0].innerHTML =
+    getMinutes(totalSeconds) + ":" + getSeconds(totalSeconds); // Initializes display
+  allDisable();
+  circle.style.animation = `animate ${totalSeconds}s linear`; // Initializes animation duration
+  runTimer();
+}
+
+function runTimer() {
+  // Is the main timer function, calls `tick` every 1000 milliseconds
+  if (pauseEl === 1) {
+    pauseCircle();
+    pauseEl = 0;
+  }
+  timer = setInterval(tick, 1000);
+}
+
+function tick() {
+  if (totalSeconds >= 2) {
+    totalSeconds--; // Decreases total seconds by one
+    countDownEl[0].innerHTML =
+      getMinutes(totalSeconds) + ":" + getSeconds(totalSeconds); // Updates display
+    allDisable();
+  } else if (totalSeconds == 1) {
+    // The timer has reached zero. Let the user start again.
+    totalSeconds--; // Decreases total seconds by one
+    countDownEl[0].innerHTML =
+      getMinutes(totalSeconds) + ":" + getSeconds(totalSeconds); // Updates display
+    pauseRestartBtn[0].innerHTML = "RESTART";
+    circle.style.strokeDashoffset = 0;
+    pauseEl = 0;
+    timersLunched = 0;
+    mainTimerFinished = 1;
+    pauseTimer();
+    allEnable();
+  } else {
+    return;
+  }
+}
+
+function pauseTimer() {
+  // Stops calling `tick` and pause circle animation
+  clearInterval(timer);
+  pauseCircle();
+  pauseEl = 1;
+}
+
+function pauseCircle() {
+  const circleStrokeDashoffset = 900;
+  let totalTime = parseInt(inputs[0].value) * 60;
+  let minutesTimerValue = parseInt(countDownEl[0].innerHTML.substring(0, 2));
+  let secondsTimerValue = parseInt(countDownEl[0].innerHTML.substring(3, 5));
+  let minutesInSeconds = minutesTimerValue * 60;
+  let pauseTotalSeconds = secondsTimerValue + minutesInSeconds;
+  let timePercentageLeft = (100 * pauseTotalSeconds) / totalTime;
+  let circleStrokeDashoffsetNeeded =
+    (circleStrokeDashoffset * timePercentageLeft) / 100;
+  let timeLeft = totalTime - (totalTime - pauseTotalSeconds);
+
+  if (pauseEl === 0) {
+    circle.style.animation = "none";
+    circle.style.strokeDashoffset = `${circleStrokeDashoffsetNeeded}`;
+  } else if (pauseEl === 1) {
+    circle.style.animation = `animate ${timeLeft}s linear`;
+    circle.style.strokeDashoffset = `${circleStrokeDashoffsetNeeded}`;
+  }
+}
+
+// Short pause timer functions
+function startShort() {
+  totalSecondsShort = inputs[1].value * 60; // Sets initial value of totalShortSeconds based on user input
+  countDownEl[1].innerHTML =
+    getMinutes(totalSecondsShort) + ":" + getSeconds(totalSecondsShort); // Initializes display
+  allDisable(); // Toggles buttons
+  runShortTimer();
+}
+
+function runShortTimer() {
+  // Is the main timer function, calls `tick` every 1000 milliseconds
+  if (pauseEl === 1) {
+    pauseCircle();
+    pauseEl = 0;
+  }
+  timerShort = setInterval(tickShortTimer, 1000);
+}
+
+function tickShortTimer() {
+  if (totalSecondsShort >= 2) {
+    totalSecondsShort--;
+    countDownEl[1].innerHTML =
+      getMinutes(totalSecondsShort) + ":" + getSeconds(totalSecondsShort); // Updates display
+    allDisable();
+    disableMainButtons();
+  } else if (totalSecondsShort == 1) {
+    // The short pause timer has reached zero, main timer resumes
+    totalSecondsShort--;
+    countDownEl[1].innerHTML =
+      getMinutes(totalSecondsShort) + ":" + getSeconds(totalSecondsShort); // Updates display
+    clearInterval(timerShort);
+    runTimer();
+    enableMainButtons();
+    timers[0].style.display = "flex";
+    timers[1].style.display = "none";
+    timers[2].style.display = "none";
+    if (colorButtonsFas[0].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStyle");
+      mainButtons[1].classList.remove("mainBtnStyle");
+    } else if (colorButtonsFas[1].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStyleBlue");
+      mainButtons[1].classList.remove("mainBtnStyleBlue");
+    } else if (colorButtonsFas[2].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStylePurple");
+      mainButtons[1].classList.remove("mainBtnStylePurple");
+    }
+  } else {
+    return;
+  }
+}
+
+// Long pause timer functions
+function startLong() {
+  totalSecondsLong = inputs[2].value * 60; // Sets initial value of totalLongSeconds based on user input
+  countDownEl[2].innerHTML =
+    getMinutes(totalSecondsLong) + ":" + getSeconds(totalSecondsLong); // Initializes display
+  allDisable();
+  runLongTimer();
+}
+
+function runLongTimer() {
+  // Is the main timer function, calls `tick` every 1000 milliseconds
+  if (pauseEl === 1) {
+    pauseCircle();
+    pauseEl = 0;
+  }
+  timerLong = setInterval(tickLongTimer, 1000);
+}
+
+function tickLongTimer() {
+  if (totalSecondsLong >= 2) {
+    totalSecondsLong--;
+    countDownEl[2].innerHTML =
+      getMinutes(totalSecondsLong) + ":" + getSeconds(totalSecondsLong); // Updates display
+    allDisable();
+    disableMainButtons();
+  } else if (totalSecondsLong == 1) {
+    // The long pause timer has reached zero, main timer resumes
+    totalSecondsLong--;
+    countDownEl[2].innerHTML =
+      getMinutes(totalSecondsLong) + ":" + getSeconds(totalSecondsLong);
+    clearInterval(timerLong);
+    enableMainButtons();
+    runTimer();
+    timers[0].style.display = "flex";
+    timers[1].style.display = "none";
+    timers[2].style.display = "none";
+    if (colorButtonsFas[0].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStyle");
+      mainButtons[2].classList.remove("mainBtnStyle");
+    } else if (colorButtonsFas[1].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStyleBlue");
+      mainButtons[2].classList.remove("mainBtnStyleBlue");
+    } else if (colorButtonsFas[2].classList.contains("fa-check")) {
+      mainButtons[0].classList.add("mainBtnStylePurple");
+      mainButtons[2].classList.remove("mainBtnStylePurple");
+    }
+  } else {
+    return;
+  }
+}
